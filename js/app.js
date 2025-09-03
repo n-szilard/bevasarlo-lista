d0c58ee30e850763709ba0c4f148166a38918d5d
n-szilard <nagyapatiszilard@turr.hu>
2025-09-03 10:53:04 +0200

    Termék tárolás

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ let itemsList = document.getElementById("itemsList");
 let sumLbl = document.getElementById("sumLbl");
 
 let items = [];
+let products = [];
 let sum = 0;
 
 addBtn.addEventListener("click", () => {
@@ -21,6 +22,23 @@ addBtn.addEventListener("click", () => {
         sum: priceField.value * countField.value
     });
 
+    let letezik = false;
+    if (products.length > 0) {
+        products.forEach((element) => {
+            if (element.name == nameField.value && element.price == priceField.value) {
+                letezik = true;
+            }
+        })
+    }
+
+    if (!letezik) {
+        products.push({
+            name: nameField.value,
+            price: priceField.value
+        })
+    }
+
+    RefreshSelect();
     RefreshTable();
     ClearForm();
     Save();
@@ -35,12 +53,19 @@ let RefreshTable = () => {
         let td3 = document.createElement("td");
         let td4 = document.createElement("td");
         let td5 = document.createElement("td");
+        let td6 = document.createElement("td");
+        let btn = document.createElement("button");
+        btn.innerHTML = "X";
 
         td3.classList.add("text-end");
         td4.classList.add("text-end");
         td5.classList.add("text-end");
-
+        td6.classList.add("text-center")
+        btn.classList.add("btn", "btn-danger");
     
+        btn.addEventListener("click", () => {
+            deleteItem(i);
+        });
         sum += items[i].sum;
 
         td1.innerHTML = i+1 + ".";
@@ -54,6 +79,8 @@ let RefreshTable = () => {
         tr.appendChild(td3)
         tr.appendChild(td4)
         tr.appendChild(td5)
+        tr.appendChild(td6)
+        td6.appendChild(btn);
 
         itemsList.appendChild(tr);
     }
@@ -74,6 +101,43 @@ let Save = () => {
 let Load = () => {
     if (localStorage.getItem("bevLista")) {
         items = JSON.parse(localStorage.getItem("bevLista"));
+    }
+}
+
+let deleteItem = (idx) => {
+    if (confirm("Biztos törölni akarod?")) {
+        items.splice(idx, 1);
+    }
+    RefreshTable();
+    Save();
+}
+
+// TODO: input+datalist, selectionchange eventlistener
+
+let RefreshSelect = () => {
+    let termekSelect = document.getElementById("termekSelect");
+    let options = termekSelect.children;
+
+    /* for (let j = 0; j < options.length; j++) {
+        options[j];
+        
+    } */
+
+    /* options.forEach((element) => {
+        if (element.value != "default") {
+            element.remove();
+        }
+    }) */
+
+    console.log(options)
+
+    for (let i = 0; i < products.length; i++) {
+        let option = document.createElement("option");
+        option.value = i;
+        option.innerHTML = products[i].name;
+        termekSelect.appendChild(option)
+
+
     }
 }
 
